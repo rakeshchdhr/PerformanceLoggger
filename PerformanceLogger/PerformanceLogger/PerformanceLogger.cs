246b6bba67ee272a2e64d1983c96cbb0dc525144
rakeshchdhr <rakesh.chaudhari02@gmail.com>
2019-08-27 15:08:27 +0530

    [PL] - added summary

--- PerformanceLogger/PerformanceLogger/PerformanceLogger.cs
+++ PerformanceLogger/PerformanceLogger/PerformanceLogger.cs
@@ -4,12 +4,15 @@ using System.Threading.Tasks;
 
 namespace PerformanceLogger
 {
+    /// <summary>
+    /// Utilty to calculate performance for operation.
+    /// </summary>
     public class PerformanceLogger
     {
         /// <summary>
         /// Logs the time taken for particular operation
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">Method Result</typeparam>
         /// <param name="type">Logger Type (use GetType())</param>
         /// <param name="methodName">Method Name</param>
         /// <param name="method">Method to log</param>
@@ -24,6 +27,14 @@ namespace PerformanceLogger
             return result;
         }
 
+        /// <summary>
+        /// Asynchronously logs the time taken for particular operation
+        /// </summary>
+        /// <typeparam name="T">Method Result</typeparam>
+        /// <param name="type">Logger Type (use GetType())</param>
+        /// <param name="methodName">Method Name</param>
+        /// <param name="method">Method to log</param>
+        /// <returns></returns>
         public async Task<T> LogAsync<T>(Type type, string methodName, Func<Task<T>> method)
         {
             var stopwatch = new Stopwatch();
